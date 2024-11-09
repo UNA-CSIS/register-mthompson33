6a2c1918accfd7a1bba6ad7c9a48db2725a66a39
mthompson33 <mthompson33@una.edu>
2024-11-09 14:03:28 -0600

    Update new_user.php

--- new_user.php
+++ new_user.php
@@ -1,17 +1,63 @@
 <?php
+
 // session start here...
+session_start();
+include "validate.php";
 
 // get all 3 strings from the form (and scrub w/ validation function)
+$user = test_input($_POST["user"]);
+$pwd = test_input($_POST["pwd"]);
+$pwdRepeat = test_input($_POST["repeat"]);
 
 // make sure that the two password values match!
+if ($pwd != $pwdRepeat) {
+    echo "passwords do not match.";
+}
+else {
+    // create the password_hash using the PASSWORD_DEFAULT argument
+    $pwdHashed = password_hash($pwd, PASSWORD_DEFAULT);
 
+    // login to the database
+    // make sure that the new user is not already in the database
+    // insert username and password hash into db (put the username in the session
+    // or make them login)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "softball";
 
-// create the password_hash using the PASSWORD_DEFAULT argument
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// login to the database
+    $sql = "SELECT id, username, password FROM users";
+    $result = $conn->query($sql);
+    $i = 1;
 
-// make sure that the new user is not already in the database
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            if ($row["username"] === $user) {
+                echo "username taken";
+            } else {
+                $i = $i + 1;
+            }
+        }
+    } else {
+        echo "0 results";
+    }
 
-// insert username and password hash into db (put the username in the session
-// or make them login)
+    $add = "INSERT INTO users (id, username, password) VALUES ('$i', '$user', '$pwdHashed')";
 
+    if ($conn->query($add) === TRUE) {
+        $_SESSION['username'] = $user;
+        header("Location: games.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
